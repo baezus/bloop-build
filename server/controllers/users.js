e6538b37898fa3be6ef68fedacf6502e18edcfd5
Baezus <elias@baez.us>
2020-12-14 16:53:09 -0500

    Got a signedJWT web token to successfully appear on a login POST route so that's cool. The backend is mostly set up, then, and I should begin building the client side.

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,39 +1,71 @@
 const db = require('../models');
+const jwt = require('jsonwebtoken');
 
-const register = (req, res) => {
-  db.User.create(req.body)
-    .then((savedUser) => {
-        res.status(201).json({ user: savedUser });
-    })
-    .catch((err) => {
-      console.log('Error in the User registration on the controllers route', err);
-      res.json({ Error: 'Unable to register the User.'});
-    });
+const register = async (req, res) => {
+  try {
+    const savedUser = await db.User.create(req.body);
+    console.log(savedUser);
+
+
+
+  } catch (err) {
+    return res.status(500).json({ 
+        "status": 500,
+        "message": "Unable to register the User."
+      });
+    }
 };
 
-const login = (req, res) => {
+const login = async (req, res) => {
   db.User.findOne({ 'username': req.body.username },
   (err, foundUser) => {
     if (err) return console.log('Could not find username in the login route.', err);
     else {
+
+      const signedJwt = jwt.sign(
+        {
+          _id: foundUser._id,
+        },
+          "bolobrazy",
+        {
+          expiresIn: "3h",
+        }
+      );
+
       let isMatch;
       if (foundUser.password === req.body.password) {
         isMatch = true;
       } else {
         isMatch = false;
       }
-      console.log(foundUser);
-      console.log(req.body);
       if (!isMatch) {
         res.send('wrong password!');
       } else {
-        res.send('u made it babe! logged in');
+        return res.status(200).json({ 
+          status: 200,
+          message: "Success",
+          id: foundUser._id,
+          signedJwt,
+        });
       }
     }
   });
 }
 
+const show = async (req, res) => {
+  try {
+    const foundUser = await db.User.findById(req.userId);
+    res.status(200).json({ status: 200, data: foundUser });
+  } catch (err) {
+    return res.status(500).json({
+      status: 500,
+      message: "Something went wrong. Please try again.",
+    });
+  }
+};
+
 module.exports = {
   register,
-  login
+  login,
+  show
 };
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const ctrl = require('../controllers');
+const authRequired = require('../middleware/authRequired');
 
 router.post('/login', ctrl.users.login);
 router.post('/register', ctrl.users.register);
+router.get('/user', authRequired, ctrl.users.show);
 
 module.exports = router;
