806a62378b7aa04773c23c6c85da57aede01514b
Baezus <elias@baez.us>
2020-12-13 23:43:12 -0500

    Added more to round out the backend server; now I need to populate the config/models/etc. and decide the DB situation.

--- server/server.js
+++ server/server.js
@@ -11,3 +11,55 @@ const io = require('socket.io')(server, {
     origin: '*'
   }
 });
+
+app.use(express.static(__dirname + '/static'))
+app.use(express.static('static'))
+
+//--------------------------------------MIDDLEWARE
+
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
+
+const corsOptions = {
+  supports_credentials: true
+};
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(morgan('tiny'));
+app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//------------------------------------WEBSOCKET CONFIG
+
+let connectedUsers = [];
+let sentMessages = [];
+
+io.on('connection', socket => {
+  const { id } = socket.client;
+  console.log(`User connected: ${id}`);
+  connectedUsers.push(id);
+  console.log(`Current users: `, connectedUsers);
+
+  socket.on('chat message', ({ nickname, msg }) => {
+    sentMessages.push({ nickname, msg });
+    db.Bloop.create({ 'sender': nickname, 'content': msg })
+      .then((savedBloop) => {
+        console.log('saved Bloop: ', savedBloop);
+      })
+      .catch((err) => {
+        console.log('Error in the controllers create', err);
+        res.json({ Error: 'Unable to create the bloop.'});
+      });
+    console.log('submitted a chat; sentMessages: ', sentMessages);
+    io.emit('chat message', { nickname, msg });
+  });
+});
+
+//------------------------------------SERVER LISTEN
+
+server.listen(port || 2737, function () {
+  console.log('Server is running on localhost: 2737!')
+});
